97773a1137996a36b16430c4c012e0ab2c255f1e
Vasileios Psychas <vaspsych@gmail.com>
2020-06-29 10:49:38 +0200

    Cors very simple very fast for now

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 /** ROUTERS */
 const indexRouter = require('./routes/index');
@@ -16,6 +17,9 @@ console.log('Server is up and running...');
 
 /** LOGS*/
 app.use(logger('dev'));
+
+/** CORS */
+app.use(cors());
 
 /**SETUP LOWDB */
 const low = require('lowdb');

0a0edb066f1eab4a0aca88b0b7c11bf14f6438f7
Vasileios Psychas <vaspsych@gmail.com>
2020-07-13 10:06:09 +0200

    Little fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,24 +3,17 @@ const createError = require('http-errors');
 
 exports.getUsers = async (req, res, next) => {
   try {
-        const users = await User.find()
-          .select("-password -__v") // hide password (!) field when sending user info to client 
-          .sort("lastName")
-          .limit(5);
-        res.send(users);
-  } 
-  catch (err) { next(err) }
+    const users = await User.find()
+      .select('-password -__v') // hide password (!) field when sending user info to client
+      .sort('lastName')
+      .limit(5);
+    res.send(users);
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.addUser = async (req, res, next) => {
-  // Assure that the user provides fields in the body...
-  if (Object.keys(req.body).length === 0) {
-    const err = createError.BadRequest(
-      `You need to send the record info in the body of the request`
-    );
-    return next(err);
-  }
-
   try {
     const userNew = await User.create(req.body);
     res.send(userNew);
@@ -32,9 +25,11 @@ exports.addUser = async (req, res, next) => {
 exports.getUser = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const user = await User.findById(req.params.id).select("-password -__v");
-    if(!user) {
-      throw new createError.NotFound()
+    const user = await User.findById(req.params.id).select(
+      '-password -__v'
+    );
+    if (!user) {
+      throw new createError.NotFound();
     }
     res.send(user);
   } catch (err) {
@@ -45,12 +40,17 @@ exports.getUser = async (req, res, next) => {
 exports.updateUser = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const userUpdated = await User.findByIdAndUpdate(id, req.body, { 
-      new: true, runValidators: true 
-    })
-    
-    if(!userUpdated) {
-      throw new createError.NotFound()
+    const userUpdated = await User.findByIdAndUpdate(
+      id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!userUpdated) {
+      throw new createError.NotFound();
     }
     res.send(userUpdated);
   } catch (err) {
